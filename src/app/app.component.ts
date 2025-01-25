f8caea3296b09c88f61932bbec6ce15e188987bf
rafaa1999 <raferhouma10@gmail.com>
2025-01-25 20:49:49 +0100

    #TESTING-234:Integrate with fake-api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'testing';
+  product:any[] = []
+  constructor(private appService:AppService ) { }
+
+  ngOnInit() {
+    this.appService.getProducts().subscribe((data:any) => {
+      this.product = data;
+      console.log(data)
+    })
+  }
+
+}
+
+@Injectable(
+  {
+    providedIn: 'root'
+  }
+)
+class AppService{
+  constructor(private http: HttpClient) {
+  }
+  // https://fakestoreapi.com/products/1
+
+  getProducts(){
+    return this.http.get("https://fakestoreapi.com/products")
+  }
+
+  getSingleProduct(id: number){
+    return this.http.get(`https://fakestoreapi.com/products/${id}`)
+  }
+
 }
